51e10d006e72ec0f530360a5159bea333fd48ccd
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-01-07 13:22:27 +0500

    Added the file extension name checker

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
-// const http = require('http');
 import fs from 'fs';
 import http from 'http';
-import sum,{a} from './app.js';
-import {mul} from './app.js';
-import {div} from './app.js';
-// import {a} from './app.js';
+import sum, { a } from './app.js';
+import { mul } from './app.js';
+import { div } from './app.js';
+import path from "path";
 
 const server = http.createServer();
-// const port =100;
+
 console.log(a);
 
+sum(2 + 3, 80);
+mul(2 + 3, 80);
+div(2 + 3, 80);
 
-sum(2+3,80);
-mul(2+3,80);
-div(2+3,80);
-let port =100;
-server.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
+const port = 100;
+server.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
 
-const data = fs.readFileSync(
-  'C:/Users/HHC/Desktop/Adeeb Bashir.txt'
-  
-);
+// ✅ Read file
+const data = fs.readFileSync('C:/Users/HHC/Desktop/Ahtsham.docx', 'utf-8');
 
-console.log("File read successfully");
+console.log('File read successfully');
 console.log(data);
+
+// ✅ Write file
+const content = 'this is the content';
+
+fs.appendFile(
+  'C:/Users/HHC/Desktop/Ahtsham.docx','\n          '+
+  content,
+  (err) => {
+    if (err) {
+      console.error('Write error:', err);
+    } else {
+      console.log('File written successfully');
+    }
+  }
+);
+const extname =path.extname("A.js");
+console.log(extname);
